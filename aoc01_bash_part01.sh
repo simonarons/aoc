855de698253bb753b37ca4b580d078ebe35aedfe
Simon Arons <simon@arons.se>
2017-12-07 00:37:41 +0100

    structure, baby

--- aoc01_bash_part01.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-digits="$1"
-[[ $digits =~ ^[0-9]+$ ]] && { sum=0; pos=0; while [[ $pos -lt ${#digits} ]]; do [[ ${digits:$pos:1} == ${digits:$pos-1:1} ]] && let sum=sum+${digits:$pos:1}; let pos++; done; echo "result: $sum"; } || echo -e "$digits is not just digits"
--- aoc01_bash_part02.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-digits="$1"
-[[ $digits =~ ^[0-9]+$ ]] && { sum=0; pos=0; while [[ $pos -lt ${#digits} ]]; do [[ $pos -lt $((${#digits}/2)) ]] && { [[ ${digits:$pos:1} == ${digits:$(($pos+${#digits}/2)):1} ]] && let sum=sum+${digits:$pos:1}; } || { [[ ${digits:$pos:1} == ${digits:$(($pos-${#digits}/2)):1} ]] && let sum=sum+${digits:$pos:1}; }; let pos++; done; echo "motherfunking bash result: $sum"; } || echo -e "that's no digits, stupid. only digits."
