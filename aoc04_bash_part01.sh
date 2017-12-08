acae65635f44d5ac294b1218110801980cfc334d
Simon Arons <simon@arons.se>
2017-12-08 10:22:18 +0100

    like 40% of all comments to commits on github must be stuff like "ioasjfaa"

--- aoc04_bash_part01.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-[[ -f "$1" ]] && { datafile="$1"; sum=0; while read line; do psw=($line); [[ ${#psw[@]} == $(echo "${psw[@]}"|tr " " "\n"|sort -u|wc -l) ]] && let sum=sum+1; done < $datafile; echo "$sum"; } || echo -e "error: no such file or something\nusage: $(basename "$0") [DATAFILE]"
