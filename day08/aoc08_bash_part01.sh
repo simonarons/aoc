9ff0fc0a356827dcbc81ea331153dd54ac2ad276
Simon Arons <simon@arons.se>
2017-12-09 03:48:26 +0100

    cleaning

--- day08/aoc08_bash_part01.sh
+++ day08/aoc08_bash_part01.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-# Advent of Code - Day 08, Part 1 - mental sanity status: decreasing.
 
-# A
-if [[ ! -f "$1" ]]; then
-	echo -en "error: input file not detected\nusage: $(basename "$0") [FILE]"
-	kill -INT $$
-fi
+# Advent of Code - Day 08, Part 1 - mental sanity status: decreasing.
+# some check if input yada yada, kill script if input not a file.
+if [[ ! -f "$1" ]]; then echo -en "error: input file not detected\nusage: $(basename "$0") [FILE]"; kill -INT $$; fi
 
 datafile="datafile.day08"
+# unset/reset/stuff
 unset register data reg reg_compare reg_modify modifier value compop comp_value
+
 # let's declare an associative array called register, 
 declare -A register;
+
 # fill that array with each "register" from the datafile, and set starting value 0
 # here we take the datafile, cut first field (delimited by space), and sort it
 # uniquely to avoid non-unique stuff
 while read line; do register["$line"]=0; done <<< "$(cut -f1 -d' ' $datafile|sort -u)"
+
 # now process the datafile 
 while read line; do
 unset data
+
 # let each line convert smoothly to an array
 data=( $line )
 
-# the datafile layout looks like this:
+# so, the datafile layout looks like this:
 #
 # g	dec	231	if	bfx	>	-10
 #
@@ -33,11 +35,14 @@ data=( $line )
 # "reg_compare"	= the register we will compare (bfx)
 # "compop"	= what comparison operator to use (greater than, '>')
 # "comp_value"	= what valu to compare $reg_compare (bfx) with (-10)
+
 reg_modify=${data[0]}; modifier=${data[1]}; value=${data[2]}; reg_compare=${data[4]}; compop=${data[5]}; comp_value=${data[6]}
+
 # cool thing, you can do substring replacing directly in a bash variable
 # so we will simply replace "inc" with a "+" and "dec" with a "-", 
 # and we run the same replace twice, since only one will actually replace it
 modifier=${modifier/inc/+}; modifier=${modifier/dec/-}
+
 # and thusly, we say (let's take the line from above as example)
 # if the value of register[bfx] is greater than -10 is true, then register[g] is register[g] + 231
 (( ${register[$reg_compare]} ${compop} ${comp_value} )) && register[$reg_modify]=$(( ${register[$reg_modify]} $modifier $value ));
@@ -46,12 +51,5 @@ done < $datafile;
 # print it somewhat nicely
 printf "\n%12s\n%s\n %-7s%8s\n%s\n" "REGISTER" "-----------------" "reg." "val." "-----------------"; for reg in "${!register[@]}"; do printf " %-7s%8s\n" $reg ${register[$reg]}; done|sort -n --key=2; printf "%s\n\n" "-----------------"
 
-
-
-
-
 # put on one, very long, line - quite obviously just stupid: 
 #datafile="datafile.day08"; unset register data reg reg_compare reg_modify modifier value compop comp_value; declare -A register; while read line; do register["$line"]=0; done <<< "$(cut -f1 -d' ' $datafile|sort -u)"; while read line; do unset data; data=( $line ); reg_modify=${data[0]}; modifier=${data[1]}; value=${data[2]}; reg_compare=${data[4]}; compop=${data[5]}; comp_value=${data[6]}; modifier=${modifier/inc/+}; modifier=${modifier/dec/-}; (( ${register[$reg_compare]} ${compop} ${comp_value} )) && register[$reg_modify]=$(( ${register[$reg_modify]} $modifier $value )); done < $datafile; printf "\n%12s\n%s\n %-7s%8s\n%s\n" "REGISTER" "-----------------" "reg." "val." "-----------------"; for reg in "${!register[@]}"; do printf " %-7s%8s\n" $reg ${register[$reg]}; done|sort -n --key=2; printf "%s\n\n" "-----------------"
-
-
-
