0fb1fdcc2f3d5c7b854a3dc4c641ef86022c1b2d
Simon Arons <simon@arons.se>
2017-12-09 15:07:00 +0100

    optimized with fold instead of grep, 0.4 sec saved, baby!

--- day04/aoc04_bash_part02.sh
+++ day04/aoc04_bash_part02.sh
@@ -3,7 +3,7 @@
 #
 # firstly, let's see if a file is passed as an argument. otherwise we'll be angry and kill ourselves.
 [[ -f "$1" ]] && datafile="$1" || {
-	echo -en "error: no real file specified.\nusage: $(basename "$0") [FILE]\n\nnow i'll kill myself.\n\tfarewell";
+	echo -en "error: no real file specified.\nusage: $(basename "$0") [FILE]\n\nnow i die.\n\tfarewell";
 	kill -INT $$; }
 # set/reset sum, and bash built-in 'SECONDS' to 0, so we can count
 sum=0; SECONDS=0;
@@ -13,14 +13,14 @@ while read line; do
 	psw=($line);
 	# here's the beauty:
 	# if the number of elements in the array is the same as the number of elements left, counted as line, after following:
-	# loop through each elemen in the current array, pipe it through 'grep' to output every character, but one line per character, 
+	# loop through each elemen in the current array, pipe it through 'fold' to output every character, but one line per character, 
 	# sort the characters, pipe them through md5sum which returns an md5 sum per line - from which we cut the last '-' part of,
 	# pipe it through 'sort', which with the '-u' switch returns only unique lines, which we count by piping them through 'wc -l'
 	# and if the number of lines is the same as elements in the array, the password is OK, i.e. no words can be rearranged to an
 	# identical word in the password.
-	(( ${#psw[@]} == $(for i in ${psw[@]}; do grep -o . <<<$i|sort|md5sum|cut -f1 -d' '; done|sort -u|wc -l) )) && let sum=sum+1;
-	#
-	# and that my friends, take 7 seconds with my i7-i4790. efficient? hell, yes!
+	# I used 'grep -o .' to get the output as 1 char per line, but it seems like 'fold -w1' might shave off ~0.4 seconds previous line is:
+	(( ${#psw[@]} == $(for i in ${psw[@]}; do fold -w1 <<<$i|sort|md5sum|cut -f1 -d' '; done|sort -u|wc -l) )) && let sum=sum+1;
+	# and that my friends, take 4.5 seconds with my i7-i4790. efficient? hell, yes!
 done < $datafile
 # and the winner is...
 echo "result: $sum (in $SECONDS sec)"
